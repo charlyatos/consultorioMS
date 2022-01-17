75822bc0ca8cc7dfe10f1e5e59059449f40a5344
Rojo <carlos-enrique.rojo@atos.net>
2022-01-17 11:52:08 -0600

    Initial commit  microservicio consultorio medico

---
+++ config-service/src/test/java/net/atos/crojo/consultorio/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.atos.crojo.consultorio.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ doctor-service/src/main/java/net/atos/crojo/consultorio/doctorservice/DoctorServiceApplication.java
@@ -0,0 +1,13 @@
+package net.atos.crojo.consultorio.doctorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorServiceApplication.class, args);
+	}
+
+}
